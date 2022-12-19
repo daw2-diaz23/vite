f263710bb325cbd3f7d8e0f2532872a487740f14
daw2-diaz23 <diazcalabuigpau@fpllefia.com>
2022-12-19 19:18:32 +0100

    Tetris paginas

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,38 @@
-const path = require('path')
+const { resolve } = require('path')
 
-export default {
-  root: path.resolve(__dirname, 'src'),
-  resolve: {
-    alias: {
-      '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
-    }
-  },
-  server: {
-    port: 8080,
-    hot: true
-  }
+module.exports = {
+    root: resolve(__dirname, './src'),
+    build: {
+        rollupOptions: {
+            /*
+                Opciones de configuración de Rollup externas, serán mergeadas con la configuracion
+                interna de Rollup de Vite.
+            */
+            input: {
+                main: resolve(__dirname, 'src/index.html'),
+                main: resolve(__dirname, 'src/Admin.html'),
+                main: resolve(__dirname, 'src/borrar.html'),
+                main: resolve(__dirname, 'src/borrarj.html'),
+                main: resolve(__dirname, 'src/editarperfil.html'),
+                main: resolve(__dirname, 'src/invitado.html'),
+                main: resolve(__dirname, 'src/jugadores.html'),
+                main: resolve(__dirname, 'src/jugadores1.html'),
+                main: resolve(__dirname, 'src/jugar.html'),
+                main: resolve(__dirname, 'src/logeado.html'),
+                main: resolve(__dirname, 'src/partida1.html'),
+                main: resolve(__dirname, 'src/partidas.html'),
+                main: resolve(__dirname, 'src/partidasj.html'),
+                main: resolve(__dirname, 'src/perfil.html'),
+                main: resolve(__dirname, 'src/raking.html'),
+                main: resolve(__dirname, 'src/rakingj.html'),
+                main: resolve(__dirname, 'src/register.html'),
+            },
+            output: {
+                dir: resolve(__dirname, 'build'), //Donde se va a crear el build de nuestra aplicacion
+                format: 'es', //Formato de ES modules
+            },
+        },
+        outDir: resolve(__dirname, 'build'),
+        //minify: false, //( Si no se quiere minificar el build) https://vitejs.dev/config/#build-minify (aplica solo a los JS no CSS)
+    },
 }
